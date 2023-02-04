b89a474ec11c8ad54d1e15d4ea9bfe383f7b7472
Abu Hozaifa <abuhozaifa8@gmail.com>
2023-02-04 11:00:31 +0530

    Annotation Declaration Example

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+	/**
+ * @ContentEntityType(
+ *   id = "my_entity_type_id",
+ *   label = @Translation("My entity type label"),
+ *   example_pair = "this_examples_value",
+ *   example_array = {
+ *     "array_key" = "array_value",
+ *     "some_other_key" = "some_other_value",
+ *   },
+ * )
+ */
